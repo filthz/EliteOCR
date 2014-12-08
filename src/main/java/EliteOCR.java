b8da42becc2de557bb916513522a8f8aa073f59d
filth <alxtbk@gmail.com>
2014-12-08 21:06:36 +0100

    initial

---
+++ src/main/java/EliteOCR.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: filth
+ * Date: 08.12.14
+ * Time: 21:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class EliteOCR {
+
+    public static void main(String args[]) {
+        System.out.println("dummy");
+    }
+}
